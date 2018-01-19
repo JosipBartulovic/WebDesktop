13c29039dab8f31a6ec609ef28f431b5a1760ba6
JosipBartulovic <josip.bartulovic3@gmail.com>
2018-01-19 22:29:07 +0100

    Cleaned up project structure

--- widget-pm/components/publisher.js
+++ widget-pm/components/publisher.js
@@ -6,13 +6,17 @@ const storage = require('./storage')
 module.exports = {
     publish: function(name){
         let url = 'http://127.0.0.1:5000/widget/publish';
+        let body = ''
         let request_data = {
             author: storage.getUser().mail, 
             name: name
         };
         request.post(url, {form: request_data})
             .on('data', (data) => {
-                console.log(data);
+                body += data;
+            })
+            .on('end', () => {
+                console.log(body);
             })
     }
 }
